6aa32e358c4e4668db1a7f693be3157e8875354d
heyahui <hyhheyahui@163.com>
2020-04-28 12:04:56 +0800

    更新课堂代码

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180206/Ch13Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180206/Ch13Activity1.java
@@ -1,16 +1,25 @@
 package cn.edu.sdwu.android02.classroom.sn170507180206;
 
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v4.content.SharedPreferencesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.jar.Manifest;
 
 public class Ch13Activity1 extends AppCompatActivity {
         private EditText ip;
@@ -24,6 +33,7 @@ public class Ch13Activity1 extends AppCompatActivity {
         SharedPreferences sharedPreference=getSharedPreferences("prefs",MODE_PRIVATE);
         ip.setText(sharedPreference.getString("ip",""));
         port.setText(sharedPreference.getString("port",""));
+
     }
 
     public void write(View view){
@@ -36,6 +46,28 @@ public class Ch13Activity1 extends AppCompatActivity {
             findOutPutString.close();
         }catch(Exception e){
             Log.e(Ch13Activity1.class.toString(),e.toString());
+        }
+    }
+
+    public void readRaw(View view){
+        Resources resources=getResources();
+        InputStream inputStream=resources.openRawResource(R.raw.read);
+
+        try{
+            int size=inputStream.available();
+            byte[] bytes=new byte[size];
+            inputStream.read(bytes);
+            String content=new String(bytes);
+            Toast.makeText(this,content,Toast.LENGTH_LONG).show();
+        }catch (Exception e){
+            Log.e(Ch13Activity1.class.toString(),e.toString());
+
+        }finally {
+            try{
+                inputStream.close();
+            }catch (Exception e){
+                Log.e(Ch13Activity1.class.toString(),e.toString());
+            }
         }
     }
     public void read(View view){
@@ -61,6 +93,105 @@ public class Ch13Activity1 extends AppCompatActivity {
         editor.putString("ip",ip.getText().toString());
         editor.putString("port",port.getText().toString());
         editor.commit();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==101){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                EditText editText=(EditText)findViewById(R.id.ch13_1_et);
+                String content=editText.getText().toString();
+                writeExternal(content);
+            }
+        }
+        if (requestCode==102){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                readExternal();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public void writeSd(View view){
+        EditText editText=(EditText)findViewById(R.id.ch13_1_et);
+        String content=editText.getText().toString();
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            // 1.判断当前用户是否已经授权过
+           int result=checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (result== PackageManager.PERMISSION_GRANTED){
+                writeExternal(content);
+            }else {
+                requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},101);
+            }
+        }else {
+            writeExternal(content);
+        }
+
+    }
+
+    private void writeExternal(String content){
+        FileOutputStream fileOutputStream=null;
+        File file=new File(Environment.getExternalStorageDirectory(),"abcde.text");
+        try{
+            file.createNewFile();
+            if(file.exists()&&file.canWrite()){
+                fileOutputStream=new FileOutputStream(file);
+                fileOutputStream.write(content.getBytes());
+            }
+        }catch(Exception e){
+            Log.e(Ch13Activity1.class.toString(),e.toString());
+    }
+       if(fileOutputStream!=null){
+           try{
+               fileOutputStream.flush();
+               fileOutputStream.close();
+           }catch(Exception e){
+               Log.e(Ch13Activity1.class.toString(),e.toString());
+           }
+
+       }
+    }
+
+    public void readSd(View view){
+        EditText editText=(EditText)findViewById(R.id.ch13_1_et);
+        String content=editText.getText().toString();
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            // 1.判断当前用户是否已经授权过
+            int result=checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE);
+            if (result== PackageManager.PERMISSION_GRANTED){
+                writeExternal(content);
+            }else {
+                requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},102);
+            }
+        }else {
+            writeExternal(content);
+        }
+
+    }
+    private void readExternal(){
+        File file=new File(Environment.getExternalStorageDirectory(),"abcde.text");
+        FileInputStream fileInputStream=null;
+
+        try{
+            if (file.exists()&&file.canRead()){
+                fileInputStream=new FileInputStream(file);
+                int size=fileInputStream.available();
+                byte[] bytes=new byte[size];
+                fileInputStream.read(bytes);
+                Toast.makeText(this,new String(bytes),Toast.LENGTH_LONG);
+            }
+
+        }catch (Exception e){
+            Log.e(Ch13Activity1.class.toString(),e.toString());
+        }finally {
+            try{
+                fileInputStream.close();
+            }catch (Exception e){
+                Log.e(Ch13Activity1.class.toString(),e.toString());
+            }
+        }
     }
 }
 
